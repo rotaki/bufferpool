789c0338dce7b3d1921a42d868a43350ffa4f099
wattlebirdaz <wattlebirdaz@gmail.com>
2024-02-25 17:58:21 -0600

    add very simple LFU eviction policy

--- src/eviction_policy.rs
+++ src/eviction_policy.rs
@@ -1,18 +1,32 @@
+// Very simple LFU eviction policy
 pub struct EvictionPolicy {
-    buffer_size: usize,
+    frequency: Vec<usize>,
 }
 
 impl EvictionPolicy {
     pub fn new(buffer_size: usize) -> Self {
-        EvictionPolicy { buffer_size }
+        EvictionPolicy {
+            frequency: vec![0; buffer_size],
+        }
     }
 
     pub fn choose_victim(&self) -> usize {
-        // index
-        0
+        let mut min = usize::MAX;
+        let mut min_index = 0;
+        for (i, &f) in self.frequency.iter().enumerate() {
+            if f < min {
+                min = f;
+                min_index = i;
+            }
+        }
+        min_index
     }
 
-    pub fn reset(&self, index: usize) {}
+    pub fn reset(&mut self, index: usize) {
+        self.frequency[index] = 0;
+    }
 
-    pub fn update(&self, index: usize) {}
+    pub fn update(&mut self, index: usize) {
+        self.frequency[index] += 1;
+    }
 }
